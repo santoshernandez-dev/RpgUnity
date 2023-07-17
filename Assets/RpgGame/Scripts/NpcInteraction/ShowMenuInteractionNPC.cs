21e489b2b0218983de557f72c37fcdd47f32672a
saints456 <snt456@hotmail.com>
2023-07-17 13:05:30 -0600

    Added changes to interact with different NPCs

--- Assets/RpgGame/Scripts/NpcInteraction/ShowMenuInteractionNPC.cs
+++ Assets/RpgGame/Scripts/NpcInteraction/ShowMenuInteractionNPC.cs
@@ -1,3 +1,4 @@
+using OpenAI;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,16 @@ using UnityEngine;
 public class ShowMenuInteractionNPC : MonoBehaviour
 {
     private GameObject canvas;
+    private GameObject chatGPT;
     private void Start()
     {
         canvas = GameObject.FindGameObjectWithTag("GameManager");
+        chatGPT = GameObject.FindGameObjectWithTag("ChatGPT");
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player") {
+            chatGPT.GetComponent<ChatGPT>().NPC = transform.gameObject;
             canvas.GetComponent<GameChatManager>().NPCCamera = transform.Find("CameraPoint").Find("Main Camera NPC").gameObject;
             canvas.GetComponent<ManageCanvas>().ActivateMenuPanel();
         }
